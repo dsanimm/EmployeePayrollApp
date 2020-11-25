d753dbf583ecadb0a39322571f57ba94e7823a08
dsanimm <dsanimm@gmail.com>
2020-11-26 04:04:51 +0530

    Retrieve from JSON Server

--- js/form_validation.js
+++ js/form_validation.js
@@ -39,15 +39,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     checkForUpdate();
 });
-
+const createOrUpdateEmployeePayroll = () => {
+    let postURL = site_properties.server_url;
+    let methodCall = "POST";
+    if (isUpdate) {
+        methodCall = "PUT";
+        postURL = postURL + employeePayrollObj.id.toString();
+    }
+    makeServiceCall(methodCall, postURL, true, employeePayrollObj)
+        .then(resposeText => {
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        })
+        .catch(error => {
+            throw error;
+        });
+}
 const save = (event) => {
     event.preventDefault();
     event.stopPropagation();
     try {
         setEmployeePayrollObject();
-        createAndUpdateStorage();
-        resetForm();
-        window.location.replace(site_properties.home_page);
+        if (site_properties.use_local_storage.match("true")) {
+            createAndUpdateStorage();
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        } else {
+            createOrUpdateEmployeePayroll();
+        }
     } catch (e) {
         return;
     }
